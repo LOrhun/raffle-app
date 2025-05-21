e44694161fcf7db08e31d1b53db990f06d74ca4b
= <orhuninan.lutfu@gmail.com>
2025-05-21 14:37:27 +0300

    Added test video

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -9,13 +9,11 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 export class SlideshowComponent implements OnInit {
   @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
 
-  // Replace this with your Google Drive video ID
-  private readonly DRIVE_VIDEO_ID = '1CUs5W8RCeL8AOA0qBgqI8GiXfTrfM0Kp';
-  videoUrl: string = '';
+  // Replace this URL with your Google Drive video URL
+  videoUrl: string = 'https://drive.google.com/file/d/0BwHmtM9HI_CSQ3BjVlpQNVJkdUk/view?usp=sharing&resourcekey=0-FulSdv2nrfdkJ7w9nBfeMg';
 
   ngOnInit() {
-    // Convert Google Drive sharing link to direct download link
-    this.videoUrl = `https://drive.google.com/uc?export=download&id=${this.DRIVE_VIDEO_ID}`;
+    // Initialize component
   }
 
   ngAfterViewInit() {
@@ -25,5 +23,16 @@ export class SlideshowComponent implements OnInit {
     video.play().catch(error => {
       console.error('Error playing video:', error);
     });
+  }
+
+  // Method to get the direct video URL from Google Drive
+  getDirectVideoUrl(shareUrl: string): string {
+    // Extract the file ID from the Google Drive share URL
+    const fileId = shareUrl.match(/[-\w]{25,}/);
+    if (fileId) {
+      // Return the direct video URL format
+      return `https://drive.google.com/uc?export=view&id=${fileId[0]}`;
+    }
+    return shareUrl;
   }
 }
